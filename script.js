3bfb79f3b5f99c9d502f1577968026d7f120b632
Abiprodigy <abateb2021@gmail.com>
2022-06-22 14:55:32 -0400

    workday scheduler final

--- script.js
+++ script.js
@@ -23,9 +23,62 @@
   $("#hour8 .description").val(localStorage.getItem("hour16"));
   $("#hour8 .description").val(localStorage.getItem("hour17"));
 
-  
+  function timeTracker() {
+    //get current number of hours.
+    var currentHour = moment().hour(); //the use of moment.js
+
+    // loop over time blocks
+    $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+
+      //check if we moved past this time, click into given classes of past, present or future!
+         if (blockHour < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+       }
+
+      else if ( blockHour === currentHour ) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+      }
+
+      else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
+  })
+
+  }
+  timeTracker(); //rerun function
+})
 
 
 
 
- })
+
+// fetch().then() // go fetch something and then do something to it
+
+
+// fetch("http://www.omdbapi.com/?t=training+day&y=2001")
+// .then(function(response) {
+// return response.json();
+// })
+// .then(function(response2) {
+//   console.log (response2)
+//   console.log (response2.runtime)
+//   console.log (response2.ratings[1].Value)
+// });
+
+
+//  https://www.google.com/
+
+
+
+//  http://www.omdbapi.com/?t=space+jam&y=1990
+
+//  http://www.omdbapi.com/? t=space+jam & y=1990
+
+//  "http://www.omdbapi.com/   ?  apikey= 58fr55897 & t=space+jam"
